48ed9f72ded071139e01745d8c2eeac0043e0c70
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-21 15:50:04 +0800

    without display

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'colorize', '~> 1.1'
+gem 'pry'
--- hànội.rb
+++ hànội.rb
@@ -1,25 +1,43 @@
 #!/usr/bin/env ruby
 
-class Tower
-  attr_reader :width, :height, :border, :corpus, :courpus_size
+require 'pry'
+require 'colorize'
 
-  def initialize(width = 5, height = 3)
+class Tower
+  attr_reader :width, :height, 
+    :border, :corpus, :courpus_size,
+    :tabs, :max_width, :number_of_towers,
+    :current_tower
+
+  attr_accessor :tower_data
+
+  def initialize(width = 5, height = 3, current_tower = 1, number_of_towers = 1)
     @width = width
     @height = height
+    @tabs = tabs
+    @number_of_towers = number_of_towers
+    @current_tower = current_tower
+    @tower_data = []
 
     calculate_geometry
   end 
 
   def draw
-    puts border
-    courpus_size.times { puts corpus }
-    puts border
+    tower_data.push border
+    courpus_size.times { tower_data.push corpus }
+    tower_data.push border
   end
 
   def calculate_geometry
     @border ||= '*' * width
     @corpus ||= '*' + ('*' * (width - 2)) + '*'
     @courpus_size ||= height - 2
+    @max_width = number_of_towers * width
+  end 
+
+  def concatenate_tabs
+    @border = tabs + @border
+    @corpus = tabs + @corpus
   end 
 end
 
@@ -30,7 +48,7 @@ class Towers
 
   attr_reader :number_of_towers, :current_tower
 
-  def initialize(n = 10)
+  def initialize(n = 5)
     @number_of_towers = n
     @current_tower = n
 
@@ -38,17 +56,17 @@ class Towers
   end 
 
   def draw
-    @current_tower = number_of_towers
+    @current_tower = 1
 
-    number_of_towers.times do |n|
+    while current_tower <= number_of_towers do 
       current_width = @@default_width * current_tower
       current_height = @@default_height * current_tower
 
-      Tower.new(current_width, current_height).draw
+      Tower.new(current_width, current_height, current_tower, number_of_towers).draw
 
-      @current_tower = n 
+      @current_tower += 1 
     end 
   end
 end 
 
-Towers.new.draw
+Towers.new
