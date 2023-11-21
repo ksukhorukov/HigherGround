d0a37efcc7bb0af4cbb914ae142ea94044c65458
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-21 23:36:04 +0800

    todo: why colorization is not working

--- hànội/towers.rb
+++ hànội/towers.rb
@@ -10,7 +10,7 @@ class Towers
   attr_reader :number_of_towers, :current_tower, :tower_color
   attr_accessor :towers_data, :maximum_shift, :current_shift
 
-  def initialize(n: 5, color: )
+  def initialize(n: 5, color: :purple)
     @number_of_towers = n
     @current_tower = n
     @towers_data = []
@@ -22,7 +22,7 @@ class Towers
 
   def draw
     puts "\n\n"
-    
+
     @current_tower = @number_of_towers
 
     while current_tower > 0 do 
